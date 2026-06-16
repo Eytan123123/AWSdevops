0d597d8f7b32b15bf878c8240af4129e886eecff
Eytan123123 <119526687+Eytan123123@users.noreply.github.com>
2026-06-16 11:25:22 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,13 +29,6 @@ terraform {
 # plan runs offline and produces meaningful output without contacting AWS.
 provider "aws" {
   region = var.aws_region
-
-  access_key = "mock_access_key"
-  secret_key = "mock_secret_key"
-
-  skip_credentials_validation = true
-  skip_metadata_api_check     = true
-  skip_requesting_account_id  = true
 }
 
 
