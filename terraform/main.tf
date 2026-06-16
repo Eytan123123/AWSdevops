17f72217fb99359637b939a93b0c1d47e9fd6962
Eytan123123 <119526687+Eytan123123@users.noreply.github.com>
2026-06-16 11:21:41 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 # Required by tflint (best practice rule "terraform_required_version")
 # Declares the minimum Terraform version this project supports
 terraform {
-  required_version = ">= 1.5.0"
+  required_version = ">= 1.5.1"
 
   # Required by tflint (best practice rule "terraform_required_providers")
   # Declares which providers and which versions this project uses
